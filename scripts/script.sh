f4df4eb6b59abfa17c4a7075603784c9c42e1452
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2026-03-09 22:55:49 +0530

    test4

--- scripts/script.sh
+++ scripts/script.sh
@@ -17,7 +17,10 @@ start() {
 
   docker rm ${IMAGE_NAME} 2> /dev/null
   docker run -d --name "${IMAGE_NAME}" -p 9200:9200  -p 9300:9300 \
-   -e "discovery.type=single-node" -e "xpack.security.enabled=false" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+   -e "discovery.type=single-node" -e "xpack.security.enabled=false" \
+   -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+   -e "ELASTIC_PASSWORD=changeme" \
+   --cgroupns=host \
     docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
 
   # Wait elastic cluster to start
@@ -64,7 +67,9 @@ start_open_search() {
 
   docker rm ${IMAGE_OPEN_SEARCH} 2> /dev/null
   docker run -d --name "${IMAGE_OPEN_SEARCH}" -p 9200:9200 -p 9600:9600 \
-   -e "discovery.type=single-node" -e "plugins.security.disabled=true" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+   -e "discovery.type=single-node" -e "plugins.security.disabled=true" \
+   -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+   --cgroupns=host \
    opensearchproject/opensearch:${OPEN_VERSION}
 
   # Wait for OpenSearch cluster to start
